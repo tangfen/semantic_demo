1c415b63c1534a60ba81957ff6af912b919b2a18
miyatang <tangfen19149@163.com>
2019-11-06 12:49:25 +0800

    test ghook

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ let uniqueRandomArray = require('unique-random-array');
 let starName = [
   'name' , 'miyatang'
 ];
+let getRandomItem = uniqueRandomArray(starName);
+
 module.exports = {
   all: starName,
-  random:uniqueRandomArray(starName)
+  random:random
+};
+
+function random(number) {
+  if(number === undefined){
+    return getRandomItem();
+  }else{
+    var randomItems = [];
+    for(var i = 1; i<number; i++){
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
 }
